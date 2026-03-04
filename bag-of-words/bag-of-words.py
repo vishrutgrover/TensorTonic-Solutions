9821f6b584456d281acb4c299e9673f37a93387d
Vishrut Grover <grovervishrut@gmail.com>
2026-03-04 13:56:20 +0530

    Update solution for bag-of-words

--- bag-of-words/bag-of-words.py
+++ bag-of-words/bag-of-words.py
@@ -5,8 +5,14 @@ def bag_of_words_vector(tokens, vocab):
     Returns: np.ndarray of shape (len(vocab),), dtype=int
     """
     # Your code here
-    d = {}
-    for i in vocab: d[i]=0
-    for i in tokens: 
-        if i in d: d[i]+=1
-    return np.asarray(list(d.values()), dtype=int)
+    # d = {}
+    # for i in vocab: d[i]=0
+    # for i in tokens: 
+    #     if i in d: d[i]+=1
+    # return np.asarray(list(d.values()), dtype=int)
+
+    tokens = np.array(tokens)
+    vocab = np.array(vocab)
+    unique, counts = np.unique(tokens, return_counts=True)
+    count_map = dict(zip(unique, counts))
+    return np.array([count_map.get(word, 0) for word in vocab], dtype=int)
