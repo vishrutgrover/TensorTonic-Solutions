be322a49e10b9b6b9aca65dd345b96bf1b93225b
Vishrut Grover <grovervishrut@gmail.com>
2026-03-04 13:43:42 +0530

    Add solution for expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def expected_value_discrete(x, p):
+    """
+    Returns: float expected value
+    """
+    # Write code here
+    x = np.asarray(x, dtype=float)
+    p = np.asarray(p, dtype=float)
+
+    if x.shape != p.shape or not np.allclose(np.sum(p), 1, atol=1e-6): raise ValueError()
+
+    return float(np.sum(x * p))
