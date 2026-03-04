853f9e188ff382392199b8556c7711ce37b6d7a6
Vishrut Grover <grovervishrut@gmail.com>
2026-03-04 13:59:43 +0530

    Update solution for bag-of-words

--- bag-of-words/bag-of-words.py
+++ bag-of-words/bag-of-words.py
@@ -11,8 +11,15 @@ def bag_of_words_vector(tokens, vocab):
     #     if i in d: d[i]+=1
     # return np.asarray(list(d.values()), dtype=int)
 
-    tokens = np.array(tokens)
-    vocab = np.array(vocab)
-    unique, counts = np.unique(tokens, return_counts=True)
-    count_map = dict(zip(unique, counts))
-    return np.array([count_map.get(word, 0) for word in vocab], dtype=int)
+    # tokens = np.array(tokens)
+    # vocab = np.array(vocab)
+    # unique, counts = np.unique(tokens, return_counts=True)
+    # count_map = dict(zip(unique, counts))
+    # return np.array([count_map.get(word, 0) for word in vocab], dtype=int)
+
+    m = {w: i for i, w in enumerate(vocab)}
+    v = np.zeros(len(vocab), dtype=int)
+    for t in tokens:
+        i = m.get(t)
+        if t in m: v[i] += 1
+    return v
