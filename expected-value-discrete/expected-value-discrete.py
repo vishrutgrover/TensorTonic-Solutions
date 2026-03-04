49821dd46c026a551f95a5d75d078e33b9c94b87
Vishrut Grover <grovervishrut@gmail.com>
2026-03-04 13:43:50 +0530

    Update solution for expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -8,6 +8,6 @@ def expected_value_discrete(x, p):
     x = np.asarray(x, dtype=float)
     p = np.asarray(p, dtype=float)
 
-    if x.shape != p.shape or not np.allclose(np.sum(p), 1, atol=1e-6): raise ValueError()
+    if x.shape != p.shape or not np.allclose(np.sum(p), 1): raise ValueError()
 
     return float(np.sum(x * p))
