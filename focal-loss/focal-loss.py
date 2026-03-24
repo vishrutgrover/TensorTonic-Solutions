364813c9b481b895ec3b08a61d4d9d816fc29802
Vishrut Grover <grovervishrut@gmail.com>
2026-03-24 19:38:44 +0530

    Update solution for focal-loss

--- focal-loss/focal-loss.py
+++ focal-loss/focal-loss.py
@@ -6,6 +6,8 @@ def focal_loss(p, y, gamma=2.0):
     """
     # Write code here
     p, y = np.array(p), np.array(y)
+    eps = 1e-12
+    p = np.clip(p, eps, 1 - eps)
     tm1, tm2 = (1-p)**gamma * y * np.log(p), p**gamma*(1-y)*np.log(1-p)
     # print(tm1, tm2)
     return -np.sum(tm1+tm2)/len(tm1)
