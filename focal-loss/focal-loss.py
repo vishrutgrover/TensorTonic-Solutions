8298713a1c7ea9183480e61db0cdb441f92173a4
Vishrut Grover <grovervishrut@gmail.com>
2026-03-24 19:33:48 +0530

    Add solution for focal-loss

---
+++ focal-loss/focal-loss.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def focal_loss(p, y, gamma=2.0):
+    """
+    Compute Focal Loss for binary classification.
+    """
+    # Write code here
+    p, y = np.array(p), np.array(y)
+    tm1, tm2 = (1-p)**gamma * y * np.log(p), p**gamma*(1-y)*np.log(1-p)
+    # print(tm1, tm2)
+    return -np.sum(tm1+tm2)/len(tm1)
