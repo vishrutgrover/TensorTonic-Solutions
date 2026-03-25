f6f7446dfe02d46bda8a685b2338bde534e3437f
Vishrut Grover <grovervishrut@gmail.com>
2026-03-25 12:33:24 +0530

    Update solution for angle-between-3d

--- angle-between-3d/angle-between-3d.py
+++ angle-between-3d/angle-between-3d.py
@@ -9,4 +9,5 @@ def angle_between_3d(v, w):
     den1, den2 = np.sum(v**2)**0.5, np.sum(w**2)**0.5
     if den1 == 0: den1 = np.nan
     if den2 == 0: den2 = np.nan
-    return np.clip(np.arccos((v@w) / (den1*den2)))
+    cost = np.clip((v@w) / (den1*den2), -1.0, 1.0)
+    return np.arccos(cost)
